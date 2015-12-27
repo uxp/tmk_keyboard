9c161d81fb87372dbefcccde7941d4bb23782c11
Howard P. Logsdon <hplogsdon@Howards-MacBook-Pro.local>
2015-12-27 10:17:36 -0700

    Rework QWERTY layout to use new macros

--- keyboard/atreus/keymap_qwerty.c
+++ keyboard/atreus/keymap_qwerty.c
@@ -6,10 +6,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      A,   S,   D,   F,   G,           H,   J,   K,   L,SCLN,
     Z,   X,   C,   V,   B,             N,   M,COMM, DOT,SLSH,
   ESC, TAB,LGUI,LSFT,BSPS,LCTL,LALT, SPC, FN0,MINS,QUOT, ENT),
+
   /* 1: punctuation and numbers */
-  FN_LAYER,                                     \
+	KEYMAP(
+   EXCL,  AT,LCBR,RCBR,VBAR,      PGUP,  P7,  P8,  P9,PAST,
+  HASH,DOLR,LPBR,RPBR, GRV,        PGDN,  P4,  P5,  P6,PPLS,
+ PERC, CAR,LBRC,RBRC,TILD,          AMPR,  P1,  P2,  P3,PSLS,
+  FN1, INS,LGUI,LSFT,BSPS,LCTL,LALT, SPC, FN0,PDOT,  P0,PEQL),
+
   /* 2: arrows and function keys */
-  LAYER_TWO
+	KEYMAP(
+    INS,HOME,  UP, END,PGUP,        UP,  F7,  F8,  F9,PAST,
+   DEL,LEFT,DOWN,RGHT,PGDN,        DOWN,  F4,  F5,  F6,PPLS,
+   NO,  NO,  NO,  NO,  NO,            NO,  F1,  F2,  F3,PSLS,
+   NO,  NO,LGUI,LSFT,BSPS,LCTL,LALT, SPC, FN0,  NO,  NO, FN3),
 };
 
 const uint16_t PROGMEM fn_actions[] = {
